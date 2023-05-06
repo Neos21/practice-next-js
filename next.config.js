feace02eda006cc91496e8026de0ddbf7c631d2c
Neos21 <neos21@gmail.com>
2023-05-06 14:57:29 +0900

    v1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',  // コレを書くことで `$ next build` により静的なファイルを生成できるようになる (`$ next export` コマンドは Deprecated)
+  distDir: 'docs',  // デフォルトでは `out/` ディレクトリに出力されるのを変更できる https://nextjs.org/docs/pages/api-reference/next-config-js/distDir
+  basePath: '/practice-next-js',  // GitHub Pages にデプロイして動作させるためベースパスを下げる https://nextjs.org/docs/pages/api-reference/next-config-js/basePath
+  trailingSlash: true  // `index.html` を生成させ 404 を回避する
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>
+        <Link href="/">Back To Index</Link>
+      </p>
+    </main>
+  );
+}
